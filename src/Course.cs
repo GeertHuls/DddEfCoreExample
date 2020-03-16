cd7dcbefdd3ebd39e22fd9910ba3cc4cecebfa84
Geert Huls <geert.huls82@gmail.com>
2020-03-16 22:32:47 +0100

    Add initial models

---
+++ src/Course.cs
@@ -0,0 +1,8 @@
+namespace DddEfCoreExample
+{
+    public class Course
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,9 @@
 using System.IO;
 using System.Reflection;
 using DbUp;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace DddEfCoreExample
 {
@@ -13,6 +15,30 @@ namespace DddEfCoreExample
             var connectionString = GetConnectionString();
             
             InitDatabase(connectionString);
+
+            ILoggerFactory loggerFactory = CreateLoggerFactory();
+
+            var optionsBuilder = new DbContextOptionsBuilder<SchoolContext>();
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLoggerFactory(loggerFactory)
+                .EnableSensitiveDataLogging();
+
+            using (var context = new SchoolContext(optionsBuilder.Options))
+            {
+                Student student = context.Students.Find(1L);
+            }
+        }
+
+        private static ILoggerFactory CreateLoggerFactory()
+        {
+            return LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information)
+                    .AddConsole();
+            });
         }
 
         private static void InitDatabase(string connectionString)
