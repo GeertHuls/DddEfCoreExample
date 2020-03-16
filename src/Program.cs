82ecad4f15246a432044dea89f41035fc6d80167
Geert Huls <geert.huls82@gmail.com>
2020-03-16 21:57:24 +0100

    Initial project setup

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace DddEfCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var connectionString = GetConnectionString();
+
+            Console.WriteLine("Hello World! " + connectionString);
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            return configuration["ConnectionString"];
+        }
+    }
+}
