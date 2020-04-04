5744587dcefd739cd814974c8f46f1f7e0910003
Geert Huls <geert.huls82@gmail.com>
2020-04-04 22:31:18 +0200

    Apply enumeration pattern to course entity

--- src/Course.cs
+++ src/Course.cs
@@ -2,6 +2,19 @@ namespace DddEfCoreExample
 {
     public class Course : Entity
     {
+        public static readonly Course Calculus = new Course(1, "Calculus");
+        public static readonly Course Chemistry = new Course(2, "Chemistry");
+
         public string Name { get; }
+
+        protected Course()
+        {
+        }
+
+        private Course(long id, string name)
+            : base(id)
+        {
+            Name = name;
+        }
     }
 }
